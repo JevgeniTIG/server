151eae49281cbcf0cc56cb65bd5ea8a2c8836778
JevgeniTIG <jastream@hotmail.com>
2021-03-03 17:30:30 +0200

    imagesLocationConstant changed to server configurations, v18

--- src/main/java/com/example/beautyspot/constants/ImagesLocationConstant.java
+++ src/main/java/com/example/beautyspot/constants/ImagesLocationConstant.java
@@ -2,8 +2,8 @@ package com.example.beautyspot.constants;
 
 public class ImagesLocationConstant {
 	//	public static final String POST_IMAGES_LOCATION = "/Users/ZEKA/Desktop/used-surf-stuff-market/client/src/assets/images/";
-//	public static final String POST_IMAGES_LOCATION = "/var/www/client/dist/client/assets/images/";
+	public static final String POST_IMAGES_LOCATION = "/var/www/client/dist/client/assets/images/";
 //	public static final String POST_IMAGES_LOCATION = "/var/www/client/src/assets/images/";
-	public static final String POST_IMAGES_LOCATION = "/var/www/client/dist/client/images-to-post/";
+//	public static final String POST_IMAGES_LOCATION = "/var/www/client/dist/client/images-to-post/";
 }
 

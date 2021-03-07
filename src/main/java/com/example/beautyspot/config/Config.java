e6f35d57d9dd4875c994f0095e2357352c4cdf12
JevgeniTIG <jastream@hotmail.com>
2021-03-07 12:31:45 +0200

    post images logic changed, server configurations, v3

--- src/main/java/com/example/beautyspot/config/Config.java
+++ src/main/java/com/example/beautyspot/config/Config.java
@@ -2,7 +2,7 @@ package com.example.beautyspot.config;
 
 public class Config {
 	public static final String HOST = "https://surfspot.ee/";
-	public static final String POST_IMAGES_LOCATION = "/var/www/uploads/";
+	public static final String POST_IMAGES_LOCATION = "https://surfspot.ee/var/www/uploads/";
 }
 
 
